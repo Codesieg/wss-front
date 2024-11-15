6d77afbd27f773b73dbb0d6bd556eb6d0aa3046c
codesieg <sieg.heart@6hotmail.fr>
2024-11-15 20:47:50 +0100

    home css

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,31 +1,31 @@
 import './home.css';
 
-// import TextTransition, { presets } from 'react-text-transition';
-// import { motion } from  "framer-motion";
 import ReactPlayer from 'react-player';
 import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next' 
 
 
 const Home = ({joinUsFromChild}) => {
     joinUsFromChild(true);
-    const { t } = useTranslation() ;
 
     
     return (
-            <div className='home-container'>
-                <Link to="/battleheights" className='video-box--title'>Battle Heights</Link>                                  
+        <div className='home-container'>
+            <div className="home-hover">
+                <Link to="/battleheights" className='video-box--title'>Battle Heights</Link>
                 <iframe className='iframe-steam' src="https://store.steampowered.com/widget/2932940/" frameborder="0" title="steam"></iframe>
-                <ReactPlayer 
+            </div>
+            <div className="video-player">
+                <ReactPlayer
                     url='/videos/battleHeigths/homepagesite.mp4'
                     className='react-player'
                     volume='0'
-                    playing='true'
-                    loop='true'
+                    playing={true}
+                    loop={true}
                     width='100%'
-                    height= '100%'
+                    height='100%'
                 />
             </div>
+        </div>
     );
 };
 
